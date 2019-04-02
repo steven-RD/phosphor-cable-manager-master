6757b7266af97c00cb16728e0e336e678e633a2a
steven-RD <48821646+steven-RD@users.noreply.github.com>
2019-04-02 13:50:22 +0800

    Update cable_state_manager_main.cpp

--- cable_state_manager_main.cpp
+++ cable_state_manager_main.cpp
@@ -6,14 +6,16 @@ int main(int argc, char**)
 {
     auto bus = sdbusplus::bus::new_default();
 
-	auto objPathInst = std::string(CABLE_OBJPATH);
+	std::string objPathInst = std::string(CABLE_OBJPATH);
 	
 	sdbusplus::server::manager::manager objManager(bus, objPathInst.c_str());// Add sdbusplus ObjectManager.
 	std::vector<std::unique_ptr<phosphor::cable::manager::Cable>> cables;
+	int cableNum = 0;
 	
-    for(int i = 1; i <= 12; i++) {
-		auto objPathInst += std::string(i);
-		cables.emplace_back(std::make_unique<phosphor::cable::manager::Cable>(bus, objPathInst.c_str()));
+    for(int i = 0; i < 12; ++i) {
+		++cableNum;
+		std::string objPath = objPathInst + std::to_string(cableNum);		
+		cables.emplace_back(std::make_unique<phosphor::cable::manager::Cable>(bus, objPath.c_str(), cableNum));
 	}
 
     bus.request_name(CABLE_BUSNAME);
