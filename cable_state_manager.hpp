7dd9b52047cd7a3ca587aa783660c58147e7ef93
steven-RD <48821646+steven-RD@users.noreply.github.com>
2019-04-02 13:49:15 +0800

    Update cable_state_manager.hpp

--- cable_state_manager.hpp
+++ cable_state_manager.hpp
@@ -1,5 +1,11 @@
-#pragma once
 
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
 #include <functional>
 #include <sdbusplus/bus.hpp>
 #include <xyz/openbmc_project/Cable/Cable/server.hpp>
@@ -27,50 +33,50 @@ class Cable : public CableInherit
     /** @brief Constructs Cable State Manager
      *
      * @param[in] bus       - The Dbus bus object
-     * @param[in] busName   - The Dbus name to own
      * @param[in] objPath   - The Dbus object path
-     */
-    Cable(sdbusplus::bus::bus& bus, const char* objPath) :
-        CableInherit(bus, objPath, true), bus(bus), fp(NULL) {    
-			if(Open() == -1) {
-				std::cerr << "Open error!" <<std::endl;
+     * @param[in] cableNum  - The cable number
+     */ 
+	Cable() = delete;
+	Cable(const Cable&) = delete;
+	Cable& operator=(const Cable&) = delete;
+	Cable(Cable&&) = delete;
+	Cable& operator=(Cable&&) = delete;
+	~Cable();	
+    Cable(sdbusplus::bus::bus& bus, const char* objPath, const uint32_t cableNum) :
+        CableInherit(bus, objPath, true), fp(NULL) { 
+		
+		if(Open() == -1) {
+			std::cerr << "Open error!" << std::endl;
 		}
 		
-		SlotAddr(ssdaddr);
-
+		slotAddr(cableNum);
 		emit_object_added();
     };
 	
-	Cable() {
-	};
-
-	~Close() {
-		Close();
-	};
+	
 	
 	int Open();
-	uint32_t GetCableData(const std::string cableName)
-	vector<string> Split(const string& info,const string& pattern);
+	uint32_t GetCableData(const std::string& cableName);
+	vector<string> Split(string& info, const string& pattern);
 	void Close();
 	
-	uint32_t CableType() const override;
-	uint32_t Present() const override;
-	uint32_t LinkSpeed() const override;
-	uint32_t LinkWidth() const override;
-	uint32_t LinkActive() const override;
-	uint32_t PartitionID() const override;
-	uint32_t Invalid() const override;
-	uint32_t UspDsp() const override;
-	uint32_t Status() const override;
+	uint32_t cableType() ;
+	uint32_t present() ;
+	uint32_t linkSpeed() ;
+	uint32_t linkWidth() ;
+	uint32_t linkActive() ;
+	uint32_t partitionID() ;
+	uint32_t invalid() ;
+	uint32_t uspDsp() ;
+	uint32_t status() ;
 	
 
-	
   private:
   
 	FILE* fp;
 	
     /** @brief Persistent sdbusplus DBus bus connection. **/
-    sdbusplus::bus::bus& bus;
+    //sdbusplus::bus::bus& bus;
 	
 
 };
