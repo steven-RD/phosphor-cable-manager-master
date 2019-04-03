686d23daf2592ea0fd595c4c943f90bcd601702d
steven-RD <48821646+steven-RD@users.noreply.github.com>
2019-04-03 13:50:03 +0800

    Update cable_state_manager.hpp

--- cable_state_manager.hpp
+++ cable_state_manager.hpp
@@ -54,15 +54,15 @@ class Cable : public CableInherit
 	uint32_t GetCableData(const std::string& cableName);
 	std::pair<std::string, uint32_t> Split(string& info, const string& pattern);
 	
-	uint32_t cableType() ;
-	uint32_t present() ;
-	uint32_t linkStatus() ;
-	uint32_t linkWidth() ;
-	uint32_t linkActive() ;
-	uint32_t partitionID() ;
-	uint32_t invalid() ;
-	uint32_t uspDsp() ;
-	uint32_t status() ;
+	uint32_t cableType() override;
+	uint32_t present() override;
+	uint32_t linkStatus() override;
+	uint32_t linkWidth() override;
+	uint32_t linkActive() override;
+	uint32_t partitionID() override;
+	uint32_t invalid() override;
+	uint32_t uspDsp() override;
+	uint32_t status() override;
 	
 
   private:
@@ -76,4 +76,5 @@ class Cable : public CableInherit
 } // namespace manager
 } // namespace Cable
 } // namespace phosphor
+
 
